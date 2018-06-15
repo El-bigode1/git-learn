2e9b80912ca814f81a93cb89d1f4fb63998c285c
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2018-06-15 16:34:29 -0300

    Add point

---
+++ src/Point.java
@@ -0,0 +1,17 @@
+public class Point{
+  public Point(intx, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int x() {
+    return x;
+  }
+
+  public int y() {
+    return y;
+  }
+
+  private int x;
+  private int y;
+}
